16c672ae2c8702db277bca6353db01e05802d6e8
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-08-23 22:31:27 +0800

    Updated module outputs.

--- outputs_cluster_disabled.tf
+++ outputs_cluster_disabled.tf
@@ -1,44 +1,44 @@
 output "cluster_disabled_replication_group_id" {
   description = "ID of the \"cluster-disabled\" replication group."
-  value       = aws_elasticache_replication_group.replication_group_cluster_disabled[0].id ? aws_elasticache_replication_group.replication_group_cluster_disabled[0].id : null
+  value       = length(aws_elasticache_replication_group.replication_group_cluster_disabled) > 0 ? aws_elasticache_replication_group.replication_group_cluster_disabled[*].id : null
 }
 
 output "cluster_disabled_replication_group_arn" {
   description = "ARN of the \"cluster-disabled\" replication group."
-  value       = aws_elasticache_replication_group.replication_group_cluster_disabled[0].arn ? aws_elasticache_replication_group.replication_group_cluster_disabled[0].arn : null
+  value       = length(aws_elasticache_replication_group.replication_group_cluster_disabled) > 0 ? aws_elasticache_replication_group.replication_group_cluster_disabled[*].arn : null
 }
 
 output "cluster_disabled_replication_group_engine_version" {
   description = "Actual engine version of the \"cluster-disabled\" replication group."
-  value       = aws_elasticache_replication_group.replication_group_cluster_disabled[0].engine_version_actual ? aws_elasticache_replication_group.replication_group_cluster_disabled[0].engine_version_actual : null
+  value       = length(aws_elasticache_replication_group.replication_group_cluster_disabled) > 0 ? aws_elasticache_replication_group.replication_group_cluster_disabled[*].engine_version_actual : null
 }
 
 output "cluster_disabled_replication_group_cluster_enabled" {
   description = "Cluster status of the \"cluster-disabled\" replication group."
-  value       = aws_elasticache_replication_group.replication_group_cluster_disabled[0].cluster_enabled ? aws_elasticache_replication_group.replication_group_cluster_disabled[0].cluster_enabled : null
+  value       = length(aws_elasticache_replication_group.replication_group_cluster_disabled) > 0 ? aws_elasticache_replication_group.replication_group_cluster_disabled[*].cluster_enabled : null
 }
 
 output "cluster_disabled_replication_group_config_endpoint" {
   description = "Configuration endpoint address of the \"cluster-disabled\" replication group."
-  value       = aws_elasticache_replication_group.replication_group_cluster_disabled[0].configuration_endpoint_address ? aws_elasticache_replication_group.replication_group_cluster_disabled[0].configuration_endpoint_address : null
+  value       = length(aws_elasticache_replication_group.replication_group_cluster_disabled) > 0 ? aws_elasticache_replication_group.replication_group_cluster_disabled[*].configuration_endpoint_address : null
 }
 
 output "cluster_disabled_replication_group_member_clusters" {
   description = "Member clusters of the \"cluster-disabled\" replication group."
-  value       = aws_elasticache_replication_group.replication_group_cluster_disabled[0].member_clusters ? aws_elasticache_replication_group.replication_group_cluster_disabled[0].member_clusters : null
+  value       = length(aws_elasticache_replication_group.replication_group_cluster_disabled) > 0 ? aws_elasticache_replication_group.replication_group_cluster_disabled[*].member_clusters : null
 }
 
 output "cluster_disabled_replication_group_primary_endpoint" {
   description = "Primary endpoint address of the \"cluster-disabled\" replication group."
-  value       = aws_elasticache_replication_group.replication_group_cluster_disabled[0].primary_endpoint_address ? aws_elasticache_replication_group.replication_group_cluster_disabled[0].primary_endpoint_address : null
+  value       = length(aws_elasticache_replication_group.replication_group_cluster_disabled) > 0 ? aws_elasticache_replication_group.replication_group_cluster_disabled[*].primary_endpoint_address : null
 }
 
 output "cluster_disabled_replication_group_reader_endpoint" {
   description = "Reader endpoint address of the \"cluster-disabled\" replication group."
-  value       = aws_elasticache_replication_group.replication_group_cluster_disabled[0].reader_endpoint_address ? aws_elasticache_replication_group.replication_group_cluster_disabled[0].reader_endpoint_address : null
+  value       = length(aws_elasticache_replication_group.replication_group_cluster_disabled) > 0 ? aws_elasticache_replication_group.replication_group_cluster_disabled[*].reader_endpoint_address : null
 }
 
 output "cluster_disabled_cache_nodes" {
   description = "Cache nodes of the \"cluster-disabled\" cluster_mode."
-  value       = aws_elasticache_cluster.cluster_disabled[0].cache_nodes ? aws_elasticache_cluster.cluster_disabled[0].cache_nodes : null
+  value       = length(aws_elasticache_cluster.cluster_disabled) > 0 ? aws_elasticache_cluster.cluster_disabled[*].cache_nodes : null
 }
--- outputs_instance.tf
+++ outputs_instance.tf
@@ -1,14 +1,14 @@
 output "instance_arn" {
   description = "ARN of the \"instance\" cluster_mode."
-  value       = aws_elasticache_cluster.cluster_instance[0].arn ? aws_elasticache_cluster.cluster_instance[0].arn : null
+  value       = length(aws_elasticache_cluster.cluster_instance) > 0 ? aws_elasticache_cluster.cluster_instance[*].arn : null
 }
 
 output "instance_actual_engine_version" {
   description = "Actual engine version of the \"instance\" cluster_mode."
-  value       = aws_elasticache_cluster.cluster_instance[0].engine_version_actual ? aws_elasticache_cluster.cluster_instance[0].engine_version_actual : null
+  value       = length(aws_elasticache_cluster.cluster_instance) > 0 ? aws_elasticache_cluster.cluster_instance[*].engine_version_actual : null
 }
 
 output "instance_cache_nodes" {
   description = "Cache nodes of the \"instance\" cluster_mode."
-  value       = aws_elasticache_cluster.cluster_instance[0].cache_nodes ? aws_elasticache_cluster.cluster_instance[0].cache_nodes : null
+  value       = length(aws_elasticache_cluster.cluster_instance) > 0 ? aws_elasticache_cluster.cluster_instance[*].cache_nodes : null
 }
