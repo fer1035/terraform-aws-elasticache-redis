476a276aea3eb7911c58b757fd705eb96ce25f35
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-09-24 00:13:13 +0800

    Updated logging config.

--- log_group.tf
+++ log_group.tf
@@ -1,5 +1,11 @@
-resource "aws_cloudwatch_log_group" "log_group" {
-  name              = "${var.redis_id}-logs"
+resource "aws_cloudwatch_log_group" "slow_logs" {
+  name              = "${var.redis_id}-slow-logs"
+  retention_in_days = var.log_group_retention
+  kms_key_id        = var.redis_kms_id
+}
+
+resource "aws_cloudwatch_log_group" "engine_logs" {
+  name              = "${var.redis_id}-engine-logs"
   retention_in_days = var.log_group_retention
   kms_key_id        = var.redis_kms_id
 }
