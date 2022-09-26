899b9b64b89c63bb2af1676ad2fec8346d7adf0f
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2022-09-26 22:08:14 +0800

    Updated examples.

--- examples/Instance/main.tf
+++ examples/Instance/main.tf
@@ -1,13 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-
-  default_tags {
-    tags = {
-      Name = "Redis-Instance"
-    }
-  }
-}
-
 module "network" {
   source  = "app.terraform.io/fer1035/network/aws"
 }
@@ -30,4 +20,39 @@ module "elasticache-redis" {
   cluster_mode     = "instance"
   redis_subnet_ids = [module.network.private_subnet_1, module.network.private_subnet_2]
   redis_sgids      = [module.security-group.security_group_id]
+}
+
+output "instance_arn" {
+  description = "ARN of the \"instance\" cluster_mode."
+  value       = module.elasticache-redis.instance_arn
+}
+
+output "instance_actual_engine_version" {
+  description = "Actual engine version of the \"instance\" cluster_mode."
+  value       = module.elasticache-redis.instance_actual_engine_version
+}
+
+output "instance_cache_nodes" {
+  description = "Cache nodes of the \"instance\" cluster_mode."
+  value       = module.elasticache-redis.instance_cache_nodes
+}
+
+output "redis_port" {
+  description = "Redis connection port."
+  value       = module.elasticache-redis.redis_port
+}
+
+output "slow_logs" {
+  description = "CloudWatch Logs group for the Redis application."
+  value       = module.elasticache-redis.slow_logs
+}
+
+output "engine_logs" {
+  description = "CloudWatch Logs group for the Redis engine."
+  value       = module.elasticache-redis.engine_logs
+}
+
+output "sns_topic" {
+  description = "SNS notification topic for the Redis application."
+  value       = module.elasticache-redis.sns_topic
 }
